7440141f3cc963c107d5b6893a660f2078307a6b
Ginru91 <dimchaba@gmail.com>
2023-05-16 20:45:50 -0400

    fixed lint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -26,6 +26,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 #  T E S T   C A S E S
 ######################################################################
 
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -150,7 +151,7 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/0"
             )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    
+
     def test_list_all_accounts(self):
         """ It should list all accounts """
 
